95de34ae4ffffc6ad889e5ab0ca93f6a71267762
samrath <samrathpanchal8878@gmail.com>
2022-11-12 15:47:02 +0530

    initial commit

---
+++ routes/profile.routes.js
@@ -0,0 +1,8 @@
+const ProfileRouter = require('express').Router();
+const ProfileController = require('../controllers/profile.controller');
+
+ProfileRouter.get('/:id?', ProfileController.getProfile);
+ProfileRouter.post('/', ProfileController.createProfile);
+ProfileRouter.patch('/', ProfileController.updateProfile);
+
+module.exports = ProfileRouter;
